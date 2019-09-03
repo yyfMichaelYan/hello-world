fc08b50ce092a818606e965ede1688fac2bb51af
yyfMichaelYan <yyfmichaelyan@gmail.com>
2019-09-03 13:04:32 -0500

    Add HelloNumbers

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int x = 0;
+		int sum = 0;
+		while ( x < 10 ){
+			sum = sum + x;
+			System.out.print(sum + " ");
+			x += 1;
+		}
+	System.out.print(5 + "10");
+	}
+}
